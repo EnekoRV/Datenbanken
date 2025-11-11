be461c470afab9ad448626d138b3361c242de069
14247030 <14247030@205-4.Fortis.local>
2025-11-11 12:29:53 +0100

    Datenbank

---
+++ src/datenbank/Datenbank.java
@@ -0,0 +1,12 @@
+package datenbank;
+
+public class Datenbank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hallooooooooooooo");
+    }
+    
+}
