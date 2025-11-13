b087969f61d35a0b487f4ab491499d9119056af8
Lennyy187 <lennard.pramhas@gmail.com>
2025-11-13 10:48:59 +0100

    Update Datenbank.java

--- src/datenbank/Datenbank.java
+++ src/datenbank/Datenbank.java
@@ -1,8 +1,12 @@
 package datenbank;
 import java.util.Scanner;
-public class Datenbank {
+public class Datenbank extends Main {
+    public void Age(){
+        Scanner s = new Scanner(System.in);
+        
     
 }
+
 
 
 
