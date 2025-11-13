4dcecf099762549f40ce8f7bd6a2bde11075fee3
EnekoRV <erv@jr-c.de>
2025-11-13 11:13:18 +0100

    Update Datenbank.java

--- src/datenbank/Datenbank.java
+++ src/datenbank/Datenbank.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Datenbank extends Main {
     Scanner s = new Scanner(System.in);
     public void Age(){
-        int Kinder = K;
-        int Erwachsene = E;
-        int Bereiche = B;
-        System.out.println("Pro Bereich kostet eine Eintrittskarte für kinder/Ermäßigte 10€ und für Erwachsene 15€");
-        System.out.println("Wie viele ermäßigte Tickets möchten Sie kaufen?")
-            Int K = in.nextInt();
         
+        System.out.println("Pro Bereich kostet eine Eintrittskarte für kinder/Ermäßigte 10€ und für Erwachsene 15€");
+        System.out.println("Wie viele ermäßigte Tickets möchten Sie kaufen?");
+            int K = s.nextInt();
+        int Kinder = K;
+        System.out.println(K);
+        //int Erwachsene = E;
+        //int Bereiche = B;
         
     
 }
@@ -17,7 +18,8 @@ public class Datenbank extends Main {
         
     }
     public void Bereiche(){
-        System.out.println("
+        System.out.println("");
+    }
 
 }
 
