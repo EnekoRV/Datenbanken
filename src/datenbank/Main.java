a3829a9071e1731073ed29e5a0fc124dd5e92787
EnekoRV <erv@jr-c.de>
2025-11-13 11:14:29 +0100

    Update Main.java

--- src/datenbank/Main.java
+++ src/datenbank/Main.java
@@ -4,6 +4,9 @@ package datenbank;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Herzlich Willkommen zu Ihrem Abenteuer im ZooTopia");
+        Datenbank d = new Datenbank();
+        d.Age();
     }
 }
+
 
