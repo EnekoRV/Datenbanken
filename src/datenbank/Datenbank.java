1765e28ef69fe023d0b338267f3d8e7dc718a4d4
Lennyy187 <lennard.pramhas@gmail.com>
2025-11-13 11:06:03 +0100

    Update Datenbank.java

--- src/datenbank/Datenbank.java
+++ src/datenbank/Datenbank.java
@@ -1,8 +1,15 @@
 package datenbank;
 import java.util.Scanner;
 public class Datenbank extends Main {
+    Scanner s = new Scanner(System.in);
     public void Age(){
-        Scanner s = new Scanner(System.in);
+        int Kinder = K;
+        int Erwachsene = E;
+        int Bereiche = B;
+        System.out.println("Pro Bereich kostet eine Eintrittskarte für kinder/Ermäßigte 10€ und für Erwachsene 15€");
+        System.out.println("Wie viele ermäßigte Tickets möchten Sie kaufen?")
+            Int K = in.nextInt();
+        
         
     
 }
@@ -13,6 +20,7 @@ public class Datenbank extends Main {
         System.out.println("
 
 }
+
 
 
 
