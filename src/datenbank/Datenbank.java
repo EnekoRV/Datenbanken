9095523894446db28a7adbb3258a9b965026980e
EnekoRV <erv@jr-c.de>
2025-11-13 10:30:56 +0100

    Update Datenbank.java

--- src/datenbank/Datenbank.java
+++ src/datenbank/Datenbank.java
@@ -1,11 +1,7 @@
 package datenbank;
 
 public class Datenbank {
-
-
-    public static void main(String[] args) {
-        System.out.println("Hallooooooooooooo lj jikhjm Hu8nd");
-    }
     
 }
+
 
