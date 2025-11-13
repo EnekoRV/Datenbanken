05a616e8e365774b7cefa0d461e33eaaa554f1ab
Laura-1206 <laurarichter915@gmail.com>
2025-11-13 10:51:22 +0100

    Update Main.java

--- src/datenbank/Main.java
+++ src/datenbank/Main.java
@@ -3,6 +3,7 @@ package datenbank;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallooooooooooooo lj jikhjm");
+        System.out.println("Herzlich Willkommen zu Ihrem Abenteuer im ZooTopia");
     }
 }
+
