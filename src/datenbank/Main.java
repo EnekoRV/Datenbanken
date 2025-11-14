2910f88f509072b1899c71caf10f3cb7a39b2f57
EnekoRV <erv@jr-c.de>
2025-11-14 12:42:03 +0100

    Update Main.java

--- src/datenbank/Main.java
+++ src/datenbank/Main.java
@@ -1,11 +1,27 @@
-
 package datenbank;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         System.out.println("Herzlich Willkommen zu Ihrem Abenteuer im ZooTopia");
         Datenbank d = new Datenbank();
-        d.Age();
-        d.Preise();
+        int i = 0;
+        while (true){
+            d.Age();
+            d.Preise();
+            d.Barcode();
+            i = i + 1;            
+            System.out.println(i);
+            
+            System.out.println("Wollen sie noch eine Einrtittskarte kaufen?");
+            String Eintritt = s.nextLine();
+            switch(Eintritt){
+                case "Nein":
+                    return;
+                case "Ja":
+            }
+        }
     }
 }
