6048ba6c8cf7ce19cd54bd3fb4e9ffa601f05400
Lennyy187 <lennard.pramhas@gmail.com>
2025-11-14 11:44:29 +0100

    Update Datenbank.java

--- src/datenbank/Datenbank.java
+++ src/datenbank/Datenbank.java
@@ -7,19 +7,25 @@ public class Datenbank extends Main {
         System.out.println("Pro Bereich kostet eine Eintrittskarte für kinder/Ermäßigte 10€ und für Erwachsene 15€");
         System.out.println("Wie viele ermäßigte Tickets möchten Sie kaufen?");
             int K = s.nextInt();
-        public int Kinder = K;
-        System.out.println("Sie haben "+ K +" ermäßigte Tickets gekauft")
+        public int AnzahlKinder = AK;
+        System.out.println("Sie haben "+ AK +" ermäßigte Tickets gekauft")
         System.out.println("Wie viele Erwachsenen Tickets möchten sie kaufen?");
             int E = s.nextInt();
-        public int Erwachsene = E;
-        System.out.println("Sie haben "+ E +" ermäßigte Tickets gekauft")
+        public int AnzahlErwachsene = AE;
+        System.out.println("Sie haben "+ AE +" ermäßigte Tickets gekauft")
         System.out.println("Wie viele Erwachsenen Tickets möchten sie kaufen?");
             int E = s.nextInt();
-        public int Bereiche = B;
-        System.out.println("Sie haben eintritt auf"+ B );
+        public int AnzahlBereiche = AB;
+        System.out.println("Sie haben eintritt auf"+ AB );
     
 }
     public void Preise(){
+        public int Gesamt;
+        public int Kinder = 10;
+        public int Erwachsene =15;
+        Gesamt = (AnzahlKinder * Kinder + AnzahlErwachsene * Erwachsene) * AnzahlBereiche;
+        System.out.println("Der Preis beträgt"+ Gesamt)
+            
         
     }
     public void Bereiche(){
@@ -27,6 +33,7 @@ public class Datenbank extends Main {
     }
 
 }
+
 
 
 
