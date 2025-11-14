04fee25814aad9d535e511cab5eafba26da6a035
EnekoRV <erv@jr-c.de>
2025-11-14 11:55:30 +0100

    Update Datenbank.java

--- src/datenbank/Datenbank.java
+++ src/datenbank/Datenbank.java
@@ -2,47 +2,31 @@ package datenbank;
 import java.util.Scanner;
 public class Datenbank extends Main {
     Scanner s = new Scanner(System.in);
+    public int AnzahlKinder;
+    public int AnzahlErwachsene;
+    public int AnzahlBereiche;
+    public int Gesamt;
+    public int Kinder = 10;
+    public int Erwachsene =15;
+    
     public void Age(){
         
         System.out.println("Pro Bereich kostet eine Eintrittskarte für kinder/Ermäßigte 10€ und für Erwachsene 15€");
         System.out.println("Wie viele ermäßigte Tickets möchten Sie kaufen?");
-            int K = s.nextInt();
-        public int AnzahlKinder = AK;
-        System.out.println("Sie haben "+ AK +" ermäßigte Tickets gekauft");
+            AnzahlKinder = s.nextInt();        
+        System.out.println("Sie haben "+ AnzahlKinder +" ermäßigte Tickets gekauft");
         System.out.println("Wie viele Erwachsenen Tickets möchten sie kaufen?");
-            int E = s.nextInt();
-        public int AnzahlErwachsene = AE;
-        System.out.println("Sie haben "+ AE +" ermäßigte Tickets gekauft");
+            AnzahlErwachsene = s.nextInt();        
+        System.out.println("Sie haben "+ AnzahlErwachsene +" ermäßigte Tickets gekauft");
         System.out.println("Wie viele Erwachsenen Tickets möchten sie kaufen?");
-            int E = s.nextInt();
-        public int AnzahlBereiche = AB;
-        System.out.println("Sie haben eintritt auf"+ AB );
+            AnzahlBereiche = s.nextInt();        
+        System.out.println("Sie haben eintritt auf"+ AnzahlBereiche );
     
 }
-    public void Preise(){
-        public int Gesamt;
-        public int Kinder = 10;
-        public int Erwachsene =15;
+    public void Preise(){      
         Gesamt = (AnzahlKinder * Kinder + AnzahlErwachsene * Erwachsene) * AnzahlBereiche;
-        System.out.println("Der Preis beträgt"+ Gesamt);
+        System.out.println("Der Preis beträgt " + Gesamt);
             
         
     }
-    public void Bereiche(){
-        System.out.println("");
-    }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
