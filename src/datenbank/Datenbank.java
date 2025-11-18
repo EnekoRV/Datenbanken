e9aef3c2bd9fadae8721822d38089fa192082959
EnekoRV <erv@jr-c.de>
2025-11-18 12:04:16 +0100

    Update Datenbank.java

--- src/datenbank/Datenbank.java
+++ src/datenbank/Datenbank.java
@@ -8,7 +8,6 @@ public class Datenbank extends Main {
     public int Gesamt;
     public int Kinder = 10;
     public int Erwachsene =15;
-    public String
     
     public void Age(){
         
@@ -18,8 +17,8 @@ public class Datenbank extends Main {
         System.out.println("Sie haben "+ AnzahlKinder +" ermäßigte Tickets gekauft");
         System.out.println("Wie viele Erwachsenen Tickets möchten sie kaufen?");
             AnzahlErwachsene = s.nextInt();        
-        System.out.println("Sie haben "+ AnzahlErwachsene +" ermäßigte Tickets gekauft");
-        System.out.println("Wie viele Erwachsenen Tickets möchten sie kaufen?");
+        System.out.println("Sie haben "+ AnzahlErwachsene +" erwachsene Tickets gekauft");
+        System.out.println("Wie viele Bereiche möchten Sie besuchen?");
             AnzahlBereiche = s.nextInt();        
         System.out.println("Sie haben eintritt auf"+ AnzahlBereiche );
         System.out.println("Wollen sie Zusätzliche Angebote kaufen?");
@@ -32,13 +31,13 @@ public class Datenbank extends Main {
         if (zusatz.equalsIgnoreCase("JA")) {
             System.out.println("1. Delfin-Show (20€ p.P.)");
             System.out.println("2. Raubtier-Show (10€ p.P.)");
-            System.out.print("Wie viele Tickets wollen Sie für die Delfin-Show kaufen?" + AnzahlDS);
+            System.out.print("Wie viele Tickets wollen Sie für die Delfin-Show kaufen?");
             int AnzahlDS = s.nextInt();
-            System.out.print("Wie viele Tickets wollen Sie für die Raubtier-Show kaufen?" + AnzahlRS);
+            System.out.print("Wie viele Tickets wollen Sie für die Raubtier-Show kaufen?");
             int AnzahlRS = s.nextInt();
 
+            int ZA = AnzahlDS * 20 + AnzahlRS * 10; 
             System.out.println(ZA);
-            int ZA = AnzahlDS * 20 + AnzahlRS * 10;    
             }
             
         
