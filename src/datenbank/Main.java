cce12b6a056bb03e16df681a1f3fc621afd9564c
Laura-1206 <laurarichter915@gmail.com>
2026-01-08 09:42:01 +0100

    Add files via upload

--- src/datenbank/Main.java
+++ src/datenbank/Main.java
@@ -1,4 +1,8 @@
-package datenbank;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ZooTopia;
 
 import java.util.Scanner;
 
@@ -7,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Herzlich Willkommen zu Ihrem Abenteuer im ZooTopia");
-        Datenbank d = new Datenbank();
+        ZooTopia d = new ZooTopia();
         int i = 0;
         while (true) {
             d.Age();
